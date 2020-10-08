32c1e021c558120def3abec1e7274847f36c5fde
Arniel <aceba1@outlook.com>
2020-10-08 17:32:45 -0400

    Small tests

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar as Toast } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  Dimensions,
+  View, 
+  StyleSheet, 
+  Platform, 
+  SafeAreaView, 
+  StatusBar, 
+  Button, 
+  Alert } from 'react-native';
+import { useDeviceOrientation, useDimensions } from '@react-native-community/hooks';
+
 
 export default function App() {
+  StatusBar.setHidden(true);
+  const {landscape} = useDeviceOrientation();
+  console.log(useDimensions());
+
+  const handlePress = () => {
+    console.log("I've been tapped!")
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button title="Click Me" onPress={
+        () => Alert.alert("My title", "My message", [
+          {text:"Yes", onPress: () => {
+            console.log("Press yes");
+          }}, 
+          {text:"No", onPress: () => {
+            console.log("Press not yes");
+          }}])}/>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#888',
+    color: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
   },
+  text: {
+    fontSize: 32
+  }
 });
