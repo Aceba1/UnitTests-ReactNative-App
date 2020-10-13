bf294807c2d1ed6f0465b7c28a9fe3753b5ba76e
Arniel <aceba1@outlook.com>
2020-10-13 17:34:47 -0400

    Prepare for *actual* hotel project merging

---
+++ app/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet, Text, View, view } from 'react-native'
+
+export default function NavBar() {
+  return (
+    <View>
+      <Image></Image>
+      <Text></Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
