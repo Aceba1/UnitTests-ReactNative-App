a2b397fee4289e4529a241942cbe2c3cb107a7df
Arniel <aceba1@outlook.com>
2020-10-13 17:34:25 -0400

    Colors!

--- app/config/colors.js
+++ app/config/colors.js
@@ -1,6 +1,17 @@
 export default {
-  primary: '#fc5c65',
-  secondary: '#4ecdc4',
+  primary: '#48f',
+  fadedPrimary: '#356',
+  fadedPrimaryHover: '#457',
+  fadedPrimaryActive: '#246',
+  primaryHover: '#69f',
+  primaryActive: '#36d',
+  secondary: '#fd3',
+  navbar: '#334',
   black: '#000',
+  white: '#fff',
+  white1: '#eee',
+  white2: '#ddd',
+  dark: '#444',
+
   
 }
