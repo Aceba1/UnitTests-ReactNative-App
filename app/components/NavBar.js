c6bd9e4b5cda602df58426374276b3df4850f1f2
Arniel <aceba1@outlook.com>
2020-10-14 22:59:05 -0400

    Create NavBar StyleSheet

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View, view } from 'react-native'
+import colors from '../config/colors'
 
 export default function NavBar() {
   return (
@@ -10,4 +11,39 @@ export default function NavBar() {
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = {
+  ...StyleSheet.create({
+    container: {
+      position: "absolute",
+      top: 0,
+      backgroundColor: colors.navbar,
+      width: "100%"
+    },
+    navBar: {
+      justifyContent: "space-around",
+      flexDirection: "row"
+    },
+    navButton: {
+      paddingVertical: 4,
+      paddingHorizontal: 16,
+      height: 30,
+      width: 150,
+    },
+    navText: {
+      color: colors.white,
+      fontSize: 20,
+      flex: 1,
+      textAlign: "center",
+    }
+  }),
+  active: StyleSheet.create({ // Active
+    button: { // Button
+      backgroundColor: colors.primary
+    }
+  }),
+  inactive: StyleSheet.create({ // Inactive
+    button: { // Button
+      backgroundColor: colors.fadedPrimary
+    }
+  })
+}
