03c7cfbc63dbbb4c0de7d2eca4d5644a26388352
Arniel <aceba1@outlook.com>
2020-10-14 22:56:50 -0400

    Render NavBar

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import {
   Text,
   SafeAreaView} from 'react-native';
 import Button from './app/components/Button';
+import NavBar from './app/components/NavBar';
 import colors from './app/config/colors';
 
 const isAndroid = Platform.OS === "android";
@@ -16,17 +17,13 @@ export default function App() {
 //  StatusBar.setHidden(true);
   //const [home, setHome] = useState(true)
 
-  StatusBar.setBackgroundColor(colors.navbar);
+  if (isAndroid)
+    StatusBar.setBackgroundColor(colors.navbar);
   StatusBar.setBarStyle("light-content")
 
   return (
     <SafeAreaView style={styles.container}>
-      <Button text="Press me!" onPress={() => Alert.alert("Critical Exception", "Reinstall OS?", [{
-        text: "ok?",
-        onPress: () => {
-          alert("Flashing to "+(isAndroid?"Apple IOS":"Android OS")+" in 15s...");
-        }
-      }])} />
+      <NavBar page="return"/>
     </SafeAreaView>
   );
 }
